7c80b2c5360ffdf413aa9a5f9aacec711aab6e55
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-17 09:49:18 -0400

    Added total

--- MathTester.java
+++ MathTester.java
@@ -3,20 +3,28 @@ public class MathTester {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber;
+        int firstNumber,secondNumber,total;
 
+    firstWord = "dog";
+    secondWord = "house";
+    firstNumber = 5;
+    secondNumber = 10;
+    total = firstNumber+secondNumber;
+    System.out.println(total);
+    total = total + 10;
+    System.out.println(total);
 
+    /*System.out.println(firstWord+secondWord);
+    System.out.println(firstWord+firstNumber);
+    System.out.println(firstWord+(firstNumber+secondNumber));
+    System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+    System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+    System.out.println("firstNumber / secondNumber is "+ (secondNumber/firstNumber));
+    System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
+    System.out.println("The Modulus of the secondNumber by the firstnumber" +
+            "is"+ (firstNumber%secondNumber));*/
 
-        firstWord = "dog";
-        secondWord = "house";
-        firstNumber = 5;
-        secondNumber = 10;
-
-
-
-        System.out.println(firstWord + secondWord);
-        System.out.println(firstWord+firstNumber);
-
+      
 
 
     }
